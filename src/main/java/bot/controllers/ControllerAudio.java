744b47fcffb238fbab1d2bd07bf1e0503d7df9ff
aleksandrandreichenko <andreichenko@web.de>
2020-11-24 00:42:18 +0300

    preparation of work + main class + controllers

---
+++ src/main/java/bot/controllers/ControllerAudio.java
@@ -0,0 +1,9 @@
+package bot.controllers;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ControllerAudio {
+
+    private final Logger LOGGER = LogManager.getLogger(ControllerAudio.class);
+}
---
+++ src/main/java/bot/services/DiscordBotService.java
@@ -0,0 +1,18 @@
+package bot.services;
+
+
+import net.dv8tion.jda.api.JDA;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DiscordBotService {
+
+    void startLoad() {
+
+    }
+
+    void off(){
+
+    }
+
+}
