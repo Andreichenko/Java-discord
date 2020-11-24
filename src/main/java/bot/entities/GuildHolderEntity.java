3cf12430496c1d27aae0f003e98aeee2fcc9123d
aleksandrandreichenko <andreichenko@web.de>
2020-11-24 20:43:22 +0300

    configuration/mongodb-config mongo config

--- src/main/java/bot/entities/GuildHolderEntity.java
+++ src/main/java/bot/entities/GuildHolderEntity.java
@@ -24,4 +24,10 @@ public class GuildHolderEntity {
         this.guildId = guildId;
         this.entityList = entityList;
     }
+
+    public void addNewEntity(Entity entity){
+        entityList.add(entity);
+    }
+
+
 }
