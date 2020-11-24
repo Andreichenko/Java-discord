9a7141537c2d4ab80c0efe94b7f81fb76fd1a814
aleksandrandreichenko <andreichenko@web.de>
2020-11-24 17:35:09 +0300

    configuration/mongodb-config mongo config

--- src/main/java/bot/entities/Entity.java
+++ src/main/java/bot/entities/Entity.java
@@ -19,4 +19,33 @@ public class Entity {
     public Entity() {
     }
 
+    public Entity(String name, String commandName, String commandArgs) {
+        this.name = name;
+        this.commandName = commandName;
+        this.commandArgs = commandArgs;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCommandName() {
+        return commandName;
+    }
+
+    public void setCommandName(String commandName) {
+        this.commandName = commandName;
+    }
+
+    public String getCommandArgs() {
+        return commandArgs;
+    }
+
+    public void setCommandArgs(String commandArgs) {
+        this.commandArgs = commandArgs;
+    }
 }
