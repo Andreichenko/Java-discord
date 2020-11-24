7c484d26f2c48f508e071bcb573798c5ccba879d
aleksandrandreichenko <andreichenko@web.de>
2020-11-24 20:30:54 +0300

    configuration/mongodb-config mongo config

--- src/main/java/bot/entities/GuildHolderEntity.java
+++ src/main/java/bot/entities/GuildHolderEntity.java
@@ -1,13 +1,17 @@
 package bot.entities;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.util.List;
 
 @Document(collection = "guildholders")
 public class GuildHolderEntity {
 
+    @Id
     public String guildId;
 
+    @Field
     public List<Entity> entityList;
 }
