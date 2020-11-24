81fb45a1bbb7f17abd0e09aefaa74bff6ed3832d
aleksandrandreichenko <andreichenko@web.de>
2020-11-24 21:53:35 +0300

    master
    + implementation

--- src/main/java/bot/services/implement/DiscordBotImplement.java
+++ src/main/java/bot/services/implement/DiscordBotImplement.java
@@ -3,10 +3,14 @@ package bot.services.implement;
 import bot.repository.EntityGuildHolderRepository;
 import bot.services.DiscordBotService;
 import net.dv8tion.jda.api.JDA;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.security.auth.login.LoginException;
 
 public class DiscordBotImplement implements DiscordBotService{
+
+    @Value("${DISCORD_BOT_KEY}")
+    private String DISCORD_BOT_KEY;
 
     @Override
     public void startLoad(EntityGuildHolderRepository entityGuildHolderRepository) throws LoginException {
