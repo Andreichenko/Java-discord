4e68904c3ddc2e64301abddf6961192fe612d33f
aleksandrandreichenko <andreichenko@web.de>
2020-11-25 00:32:40 +0300

    feature/unicode-emotion thumbsup, thumbsdown enviroments variables = null
    getting a variable by channel owner
    responce text

---
+++ src/main/java/bot/utils/ChannelTextResponses.java
@@ -0,0 +1,8 @@
+package bot.utils;
+
+public class ChannelTextResponses {
+
+    public static final String NOT_CONNECTED_TO_VOICE_MESSAGE = "**You need to be in a voice channel.**";
+    public static final String ECHO_COMMAND_NO_ARGS = "**You need to provide some text to echo.**";
+    public static final String ALIAS_CANT_BE_CREATED_COMMAND_NOT_FOUND = "**%s is not a command**";
+}
