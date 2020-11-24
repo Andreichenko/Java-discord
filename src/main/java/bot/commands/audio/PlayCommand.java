a9ce34a4806ca23af23b89fcc3873d707c0eae57
aleksandrandreichenko <andreichenko@web.de>
2020-11-24 22:00:24 +0300

    master
    + implementation
    
    + audio
    + commands from discrod implementation jagrosh

---
+++ src/main/java/bot/commands/audio/PlayCommand.java
@@ -0,0 +1,17 @@
+package bot.commands.audio;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class PlayCommand extends Command{
+
+    private final Logger LOGGER = LogManager.getLogger(PlayCommand.class);
+
+    @Override
+    protected void execute(CommandEvent event) {
+
+
+    }
+}
---
+++ src/main/java/bot/commands/text/TextCommand.java
@@ -0,0 +1,12 @@
+package bot.commands.text;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+public class TextCommand extends Command{
+
+    @Override
+    protected void execute(CommandEvent event) {
+
+    }
+}
