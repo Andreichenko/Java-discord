0deae08cabf464a6a42ec865b794f99d1e5d52c2
aleksandrandreichenko <andreichenko@web.de>
2020-11-24 03:48:49 +0300

    configuration/mongodb-config mongo config

--- src/main/java/bot/repository/MongoConfig.java
+++ src/main/java/bot/repository/MongoConfig.java
@@ -2,7 +2,7 @@ package bot.repository;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import com.mongodb.lang.NonNull;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
@@ -12,12 +12,12 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
     private static final String MONGODB_VALUE="mongodb://localhost:27017";
 
     @Override
-    protected @NonNull String getDatabaseName() {
+    protected @NotNull String getDatabaseName() {
         return "discord-base";
     }
 
     @Override
-    public MongoClient mongoClient(){
+    public @NotNull MongoClient mongoClient(){
         return MongoClients.create(MONGODB_VALUE);
     }
 }
