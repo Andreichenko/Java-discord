ca252d0c9250b140f06f19ba3ca051b6484ff913
aleksandrandreichenko <andreichenko@web.de>
2020-11-26 01:22:13 +0300

    feature/spring-configuration
    added config settings
    audio controller has been created
    final changes
    + refactoring and renaming

--- src/main/java/bot/commands/alias/Alias.java
+++ src/main/java/bot/commands/alias/Alias.java
@@ -2,6 +2,9 @@ package bot.commands.alias;
 
 import bot.entities.AliasEntity;
 import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandClient;
+import com.jagrosh.jdautilities.command.CommandEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.data.annotation.Transient;
@@ -17,16 +20,29 @@ public class Alias extends AliasEntity{
     public Alias() {
     }
 
-    public Alias(String name, String commandName, String commandArgs, Command command) {
-        super(name, commandName, commandArgs);
+    public Alias(AliasEntity aliasEntity, Command command){
+        this(aliasEntity.getAliasName(), aliasEntity.getAliasCommandArgs(), command);
+    }
+
+
+    public Alias(String aliasName, String aliasCommandArgs, Command command){
+        super(aliasName, aliasCommandArgs, command.getName());
         this.command = command;
     }
 
+    public void execute(MessageReceivedEvent event, CommandClient commandClient) {
+        LOGGER.info("Executing alias for command {}", aliasName);
+        command.run(new CommandEvent(event, aliasCommandArgs, commandClient));
+    }
     public Alias(Command command) {
         this.command = command;
     }
 
     public Command getCommand() {
         return command;
+    }
+
+    public String getAliasName() {
+        return aliasName;
     }
 }
