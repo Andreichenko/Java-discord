af498321b761a0bc45ff0d56ba9b9c77f9f0fd91
aleksandrandreichenko <andreichenko@web.de>
2020-11-27 00:01:36 +0300

    feature/player-for-test-from-lava
    Add ability to play top of the queue

--- src/main/java/bot/commands/audio/utils/TrackSchedulers.java
+++ src/main/java/bot/commands/audio/utils/TrackSchedulers.java
@@ -1,5 +1,6 @@
 package bot.commands.audio.utils;
 
+import com.google.common.collect.EvictingQueue;
 import com.sedmelluq.discord.lavaplayer.player.event.AudioEventAdapter;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import org.apache.logging.log4j.LogManager;
@@ -12,4 +13,16 @@ public class TrackSchedulers extends AudioEventAdapter{
 
     private List<AudioTrack> queue = new ArrayList<>();
     private final Logger LOGGER = LogManager.getLogger(TrackSchedulers.class);
+
+    private final EvictingQueue<AudioTrack> historyQueue = EvictingQueue.create(40);
+
+    public void queue(AudioTrack track, boolean queueFirst){
+
+    }
+    public void clearQueue(){
+
+    }
+    public void setQueue(List<AudioTrack> queue){
+
+    }
 }
