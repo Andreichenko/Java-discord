d1a38b23d01897ab84214a4190ac74cd01c84638
aleksandrandreichenko <andreichenko@web.de>
2020-11-26 13:09:52 +0300

    text command

--- src/main/java/bot/commands/text/TextCommand.java
+++ src/main/java/bot/commands/text/TextCommand.java
@@ -3,10 +3,22 @@ package bot.commands.text;
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 
+import static bot.utils.ChannelTextResponses.ECHO_COMMAND_NO_ARGS;
+
 public class TextCommand extends Command{
 
     @Override
     protected void execute(CommandEvent event) {
 
+        String textReturn = event.getArgs();
+
+        if (textReturn.isEmpty()){
+
+            event.getChannel().sendMessage(ECHO_COMMAND_NO_ARGS).queue();
+            return;
+        }
+
+        event.getChannel().sendMessage(textReturn).queue();
     }
+
 }
