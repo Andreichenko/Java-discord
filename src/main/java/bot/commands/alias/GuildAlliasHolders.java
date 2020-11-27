5a8c5bc6ca6cde5e7a66fed270ecd749af12965c
aleksandrandreichenko <andreichenko@web.de>
2020-11-27 21:26:34 +0300

    feature/player-for-test-from-lava
    commandline

--- src/main/java/bot/commands/alias/GuildAlliasHolders.java
+++ src/main/java/bot/commands/alias/GuildAlliasHolders.java
@@ -1,10 +1,53 @@
 package bot.commands.alias;
 
+import bot.entities.AliasEntity;
 import bot.entities.GuildHolderEntity;
+import com.jagrosh.jdautilities.command.CommandClient;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+import java.util.HashMap;
+import java.util.List;
 
 public class GuildAlliasHolders extends GuildHolderEntity{
 
 
+
+    private final HashMap<String, Alias> aliasNameToAliasObjects = new HashMap<>();
+
     public GuildAlliasHolders() {
     }
+
+    public GuildAlliasHolders(String guildId){
+
+    }
+
+    public GuildAlliasHolders(String guildId, List<AliasEntity> aliasEntityList){
+
+    }
+
+    public boolean doesAliasExistForCommand(String command){
+        return true;
+    }
+
+    public void executeAlias(String command, MessageReceivedEvent event, CommandClient commandClient){
+
+    }
+    public void addCommandWithAlias(String command, Alias alias){
+
+    }
+
+    public void removeCommandWithAlias(String command){
+
+    }
+
+    public Alias getCommandWithAlias(String command){
+
+        return null;
+    }
+
+    public HashMap<String, Alias> getAliasNameToAliasObject(){
+
+        return null;
+    }
+
 }
