d4e4a291e7cbd8985437b246dd577ab730cfa4f7
aleksandrandreichenko <andreichenko@web.de>
2020-11-30 00:53:51 +0300

    feature/player-for-test-from-lava
    create alias for a command from jagrosh

--- src/main/java/bot/commands/alias/AliasCreateCommands.java
+++ src/main/java/bot/commands/alias/AliasCreateCommands.java
@@ -1,4 +1,20 @@
 package bot.commands.alias;
 
-public class AliasCreateCommands {
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Set;
+
+public class AliasCreateCommands extends Command {
+
+    private final Logger LOGGER = LogManager.getLogger(AliasCreateCommands.class);
+
+    private Set<String> allCurrentCommandNames;
+
+    @Override
+    protected void execute(CommandEvent event) {
+
+    }
 }
