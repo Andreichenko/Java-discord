562ca94a6bdc803aeb745917aceaff431f2ad5ef
aleksandrandreichenko <andreichenko@web.de>
2020-12-01 21:05:27 +0300

    feature/discord-client voice implementation

--- src/main/java/bot/utils/ChannelTextResponses.java
+++ src/main/java/bot/utils/ChannelTextResponses.java
@@ -10,5 +10,6 @@ public class ChannelTextResponses {
     public static final String LOOP_DISABLED = "**Loop disabled!**";
     public static final String NO_HISTORY_TO_SHOW = "**There is no history to show.**";
     public static final String CANT_DISPLAY_QUEUE_PAGE = "**Cannot display that page**";
+    public static final String BOT_NOT_CONNECTED_TO_VOICE = "**Not currently connected to the voice channel**";
     public static final String HOW_TO_MAKE_ALIAS = "`-aliascreate ALIAS_NAME <Command to run when ALIAS_NAME is called>`";
 }
