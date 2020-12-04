634b25204363168a2add81ca27f464202be523f7
aleksandrandreichenko <andreichenko@web.de>
2020-12-04 22:39:32 +0300

    feature/discord-client
    in this update section we will collect the track scheduler from the library and implement the interface
    
    Some methods in class should be redefined to transfer data from methods and redefine them into used classes

--- src/main/java/bot/commands/audio/utils/TrackSchedulers.java
+++ src/main/java/bot/commands/audio/utils/TrackSchedulers.java
@@ -3,6 +3,7 @@ package bot.commands.audio.utils;
 import com.google.common.collect.EvictingQueue;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.player.event.AudioEventAdapter;
+import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
 import org.apache.logging.log4j.LogManager;
@@ -34,5 +35,19 @@ public class TrackSchedulers extends AudioEventAdapter{
     @Override
     public void onTrackEnd(AudioPlayer player, AudioTrack track, AudioTrackEndReason endReason){
 
+    }
+
+    @Override
+    public void onTrackException(AudioPlayer player, AudioTrack track, FriendlyException exception){
+
+    }
+
+    @Override
+    public void onTrackStuck(AudioPlayer player, AudioTrack track, long thresholdMs){
+
+    }
+
+    public EvictingQueue<AudioTrack> getHistory(){
+        return historyQueue;
     }
 }
