9b53a4f753e3ce8e995e5ebbcfdd1c4122465095
aleksandrandreichenko <andreichenko@web.de>
2020-12-06 22:57:26 +0300

    feature/discord-client
    in this update section we create listeners for bot commands

--- src/main/java/bot/commands/audio/SkipToCommand.java
+++ src/main/java/bot/commands/audio/SkipToCommand.java
@@ -2,6 +2,7 @@ package bot.commands.audio;
 
 import bot.commands.audio.utils.AudioPlayerSendHandler;
 import bot.commands.audio.utils.TrackSchedulers;
+import bot.commands.audio.utils.VoiceChannel;
 import bot.utils.UnicodeMotion;
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
@@ -26,8 +27,32 @@ public class SkipToCommand extends Command{
 
         AudioPlayerSendHandler audioPlayerSendHandler;
 
-        TrackSchedulers trackScheduler = audioPlayerSendHandler.getTrackScheduler();
+        try {
+
+            audioPlayerSendHandler = VoiceChannel.getAudioPlayerSendHandler(event.getJDA(), event.getGuild().getId());
+
+        } catch(IllegalArgumentException e) {
+
+            event.getChannel().sendMessage("**Not currently connected to the voice channel**").queue();
+
+            return;
+        }
+
         int elementSkipTo;
+
+        try {
+
+            elementSkipTo = Integer.parseInt(event.getArgs());
+
+        } catch(NumberFormatException e) {
+
+            event.getChannel().sendMessage("**You need to provide a number to skip to.**").queue();
+
+            return;
+        }
+
+        TrackSchedulers trackScheduler = audioPlayerSendHandler.getTrackScheduler();
+
         List<AudioTrack> queue = trackScheduler.getQueue();
         List<AudioTrack> sublistQueue = queue.subList(elementSkipTo - 1, queue.size());
 
