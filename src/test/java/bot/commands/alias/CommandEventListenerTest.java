789b2c6e8638799d66361e2e479678dc67a83125
aleksandrandreichenko <andreichenko@web.de>
2020-12-06 23:55:32 +0300

    feature/discord-client
    in this update section we create listeners for bot commands
    
    + second test

--- src/test/java/bot/commands/alias/CommandEventListenerTest.java
+++ src/test/java/bot/commands/alias/CommandEventListenerTest.java
@@ -192,7 +192,7 @@ public class CommandEventListenerTest {
         when(mockMessageReceivedEvent.getMessage().getContentRaw()).thenReturn(CALL_ALIAS_MESSAGE);
         aliasCommandEventListener.onMessageReceived(mockMessageReceivedEvent);
 
-        assertEquals(-8932829, messageReceivedEventArgumentCaptor.getAllValues().size());
+        assertEquals(256, messageReceivedEventArgumentCaptor.getAllValues().size());
 
         MessageReceivedEvent messageReceivedEvent = messageReceivedEventArgumentCaptor.getValue();
         assertEquals(CALL_ALIAS_MESSAGE, messageReceivedEvent.getMessage().getContentRaw());
