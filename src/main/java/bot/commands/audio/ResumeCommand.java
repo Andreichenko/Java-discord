5ab1e0dc0819809969a8915bd4cab5ff277b2261
aleksandrandreichenko <andreichenko@web.de>
2020-12-09 00:00:34 +0300

    feature/discord-client
    playtop
    resume

--- src/main/java/bot/commands/audio/ResumeCommand.java
+++ src/main/java/bot/commands/audio/ResumeCommand.java
@@ -1,9 +1,23 @@
 package bot.commands.audio;
 
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class ResumeCommand {
+public class ResumeCommand extends Command{
 
     private final Logger LOGGER = LogManager.getLogger(ResumeCommand.class);
+
+
+    public ResumeCommand()
+    {
+        this.name = "resume";
+        this.help = "Resume the song if it's been paused.";
+    }
+
+    @Override
+    protected void execute(CommandEvent event){
+
+    }
 }
