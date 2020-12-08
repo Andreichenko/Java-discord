6b5047d761a34e30e8f040bc6f1b42300fafcac1
aleksandrandreichenko <andreichenko@web.de>
2020-12-08 23:59:17 +0300

    feature/discord-client
    playtop
    resume

--- src/main/java/bot/commands/audio/ResumeCommand.java
+++ src/main/java/bot/commands/audio/ResumeCommand.java
@@ -1,4 +1,9 @@
 package bot.commands.audio;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class ResumeCommand {
+
+    private final Logger LOGGER = LogManager.getLogger(ResumeCommand.class);
 }
