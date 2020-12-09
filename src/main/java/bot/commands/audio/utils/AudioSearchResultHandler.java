6852a9a457a1c513d88abbdc8e5102a89dd4c74b
aleksandrandreichenko <andreichenko@web.de>
2020-12-10 00:29:24 +0300

    feature/audio-implementation
    we use the class to implement the audio track search to test the stream

--- src/main/java/bot/commands/audio/utils/AudioSearchResultHandler.java
+++ src/main/java/bot/commands/audio/utils/AudioSearchResultHandler.java
@@ -4,8 +4,29 @@ import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
 import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
 import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class AudioSearchResultHandler implements AudioLoadResultHandler {
+
+    private final Logger LOGGER = LogManager.getLogger(AudioSearchResultHandler.class);
+
+    private final TrackSchedulers trackSchedulers;
+    private final AudioPlayerSendHandler audioPlayerSendHandler;
+    private final String argument;
+    private final boolean playTop;
+    private final MessageChannel channel;
+
+    AudioSearchResultHandler(TrackSchedulers trackSchedulers, AudioPlayerSendHandler audioPlayerSendHandler,
+                             MessageChannel channel, String argument, boolean playTop){
+
+        this.trackSchedulers = trackSchedulers;
+        this.audioPlayerSendHandler = audioPlayerSendHandler;
+        this.argument = argument;
+        this.playTop = playTop;
+        this.channel = channel;
+    }
 
 
     @Override
