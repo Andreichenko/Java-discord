ed74ca00bce7642ddbbc25c3a13c86987160be35
aleksandrandreichenko <andreichenko@web.de>
2020-12-10 23:53:30 +0300

    feature/audio-implementation
    new command

---
+++ src/main/java/bot/commands/audio/ShufflesCommand.java
@@ -0,0 +1,12 @@
+package bot.commands.audio;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+public class ShufflesCommand extends Command{
+
+    @Override
+    protected void execute(CommandEvent event) {
+
+    }
+}
