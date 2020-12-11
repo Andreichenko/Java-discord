650a05b9ac11735570c09d556f511fdcdf646710
aleksandrandreichenko <andreichenko@web.de>
2020-12-12 01:53:16 +0300

    feature/audio-implementation
    check and report permission problems when joining voice channel

--- src/main/java/bot/utils/EmBuilder.java
+++ src/main/java/bot/utils/EmBuilder.java
@@ -5,6 +5,7 @@ import com.jagrosh.jdautilities.command.CommandEvent;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import net.dv8tion.jda.api.EmbedBuilder;
 
+import java.awt.*;
 import java.util.List;
 import java.util.Random;
 
@@ -18,10 +19,20 @@ public class EmBuilder {
     public static EmbedBuilder createEmbedBuilder( boolean totalTime, TrackSchedulers trackSchedulers, List<AudioTrack> queue,
                                                    CommandEvent event){
 
+        double totalPages = Math.ceil((double) queue.size() / 10);
+
+        Random rand = new Random();
+        float g = rand.nextFloat();
+        float b = rand.nextFloat();
+        float a = rand.nextFloat();
+        float k = rand.nextFloat();
+        float r = rand.nextFloat();
+// TODO need compare some value with class variables
+        Color randomColor = new Color(r, g, b;
         return null;
     }
 
-    Random rand = new Random();
+
 
     private static Integer getPageNumber(CommandEvent event, double totalPages){
 
