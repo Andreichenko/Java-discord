cd9f75d50fa817c23774af6e8260eed2ba77aa68
aleksandrandreichenko <andreichenko@web.de>
2020-12-12 01:29:24 +0300

    feature/audio-implementation
    check and report permission problems when joining voice channel

--- src/main/java/bot/commands/audio/JoinCommand.java
+++ src/main/java/bot/commands/audio/JoinCommand.java
@@ -1,7 +1,11 @@
 package bot.commands.audio;
 
+import bot.commands.audio.utils.VoiceChannel;
+import bot.utils.ChannelTextResponses;
 import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import net.dv8tion.jda.api.exceptions.InsufficientPermissionException;
 
 /**
  * The audio player manager that the audio player will be created from
@@ -14,6 +18,22 @@ public class JoinCommand extends Command{
         this.playerManager = playerManager;
         this.name = "join";
         this.help = "Joins the voice channel that the user is currently connected to";
+    }
+
+    @Override
+    protected void execute(CommandEvent event) {
+
+        try {
+            VoiceChannel.joinVoiceChannel(event.getMember(), event.getGuild(), playerManager);
+
+        } catch(IllegalArgumentException e) {
+
+            event.getChannel().sendMessage(ChannelTextResponses.NOT_CONNECTED_TO_VOICE_MESSAGE).queue();
+
+        } catch(InsufficientPermissionException e) {
+
+            event.getChannel().sendMessage(ChannelTextResponses.DONT_HAVE_PERMISSION_TO_JOIN_VOICE_CHANNEL).queue();
+        }
     }
 
 }
