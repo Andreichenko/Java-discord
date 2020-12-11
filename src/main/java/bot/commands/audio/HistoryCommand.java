f60ebb307b423a64a7da8233faf6a51107ec7d58
aleksandrandreichenko <andreichenko@web.de>
2020-12-12 01:35:09 +0300

    feature/audio-implementation
    check and report permission problems when joining voice channel

--- src/main/java/bot/commands/audio/HistoryCommand.java
+++ src/main/java/bot/commands/audio/HistoryCommand.java
@@ -1,4 +1,16 @@
 package bot.commands.audio;
 
-public class HistoryCommand {
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+/**
+ * Add history command
+ */
+public class HistoryCommand extends Command{
+
+    @Override
+    protected void execute(CommandEvent event) {
+
+    }
 }
