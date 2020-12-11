eb19c9731d0129ce70ed13fbe61592f0fef30293
aleksandrandreichenko <andreichenko@web.de>
2020-12-12 01:27:29 +0300

    feature/audio-implementation
    check and report permission problems when joining voice channel

--- src/main/java/bot/commands/audio/JoinCommand.java
+++ src/main/java/bot/commands/audio/JoinCommand.java
@@ -1,4 +1,19 @@
 package bot.commands.audio;
 
-public class JoinCommand {
+import com.jagrosh.jdautilities.command.Command;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+
+/**
+ * The audio player manager that the audio player will be created from
+ */
+public class JoinCommand extends Command{
+
+    private final AudioPlayerManager playerManager;
+
+    public JoinCommand(AudioPlayerManager playerManager){
+        this.playerManager = playerManager;
+        this.name = "join";
+        this.help = "Joins the voice channel that the user is currently connected to";
+    }
+
 }
