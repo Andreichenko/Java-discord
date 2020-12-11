e9b805f3e45dc079cd7b490650d75e9ee80208b0
aleksandrandreichenko <andreichenko@web.de>
2020-12-12 01:30:33 +0300

    feature/audio-implementation
    check and report permission problems when joining voice channel

--- src/main/java/bot/commands/audio/ClearQueueCommand.java
+++ src/main/java/bot/commands/audio/ClearQueueCommand.java
@@ -1,4 +1,21 @@
 package bot.commands.audio;
 
-public class ClearQueueCommand {
+import bot.commands.audio.utils.AudioPlayerSendHandler;
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+public class ClearQueueCommand extends Command {
+
+    public ClearQueueCommand(){
+
+        this.name = "clear";
+        this.help = "Clear the queue for this server";
+    }
+
+    @Override
+    protected void execute(CommandEvent event) {
+
+        AudioPlayerSendHandler audioPlayerSendHandler;
+
+    }
 }
