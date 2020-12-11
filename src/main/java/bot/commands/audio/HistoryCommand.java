aa9205250029c4a3761cb445b33dfa12bc2e1e3f
aleksandrandreichenko <andreichenko@web.de>
2020-12-12 01:38:05 +0300

    feature/audio-implementation
    check and report permission problems when joining voice channel

--- src/main/java/bot/commands/audio/HistoryCommand.java
+++ src/main/java/bot/commands/audio/HistoryCommand.java
@@ -1,16 +1,41 @@
 package bot.commands.audio;
 
 
+import bot.commands.audio.utils.AudioPlayerSendHandler;
+import com.google.common.collect.EvictingQueue;
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import net.dv8tion.jda.api.managers.AudioManager;
 
 /**
  * Add history command
  */
 public class HistoryCommand extends Command{
 
+    public HistoryCommand(){
+
+        this.name = "history";
+        this.help = "Show the play history";
+    }
+
+
     @Override
     protected void execute(CommandEvent event) {
+
+        AudioManager audioManager = event.getGuild().getAudioManager();
+
+        AudioPlayerSendHandler audioPlayerSendHandler = (AudioPlayerSendHandler) audioManager.getSendingHandler();
+
+        if (audioPlayerSendHandler == null){
+            return;
+        }
+
+        EvictingQueue<AudioTrack> history = audioPlayerSendHandler.getTrackScheduler().getHistory();
+
+        if (history.size() == 0){
+            return;
+        }
 
     }
 }
