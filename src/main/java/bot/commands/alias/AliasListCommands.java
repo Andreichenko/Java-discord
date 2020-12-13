0e91caff42dfc9112f9b099e4e0367557e06b1b3
aleksandrandreichenko <andreichenko@web.de>
2020-12-13 15:02:03 +0300

    feature/audio-implementation
    commands test

--- src/main/java/bot/commands/alias/AliasListCommands.java
+++ src/main/java/bot/commands/alias/AliasListCommands.java
@@ -1,17 +1,28 @@
 package bot.commands.alias;
 
 
+import bot.listeners.CommandEventListener;
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 
 //Splitting a message so that it can display multiple aliases
 public class AliasListCommands extends Command{
 
-    public AliasListCommands() {
+    private final CommandEventListener commandEventListener;
+
+    public AliasListCommands(CommandEventListener commandEventListener){
+        this.name = "aliaslist";
+        this.aliases = new String[]{"al"};
+        this.help = "List all the aliases for this server";
+        this.commandEventListener = commandEventListener;
     }
 
     @Override
     protected void execute(CommandEvent event) {
+
+        String guildId = event.getGuild().getId();
+        GuildAlliasHolders guildAliasHolder = commandEventListener.getGuildAliasHolderForGuildWithId(guildId);
+
 
     }
 }
