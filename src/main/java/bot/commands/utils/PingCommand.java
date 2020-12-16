fd3e39160b00f656494adef16165d167e3f0334b
aleksandrandreichenko <andreichenko@web.de>
2020-12-16 20:37:33 +0300

    feature/audio-implementation
    added a commands for ping and queue

--- src/main/java/bot/commands/utils/PingCommand.java
+++ src/main/java/bot/commands/utils/PingCommand.java
@@ -2,6 +2,9 @@ package bot.commands.utils;
 
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
+import net.dv8tion.jda.api.EmbedBuilder;
+
+import java.awt.*;
 
 public class PingCommand extends Command {
 
@@ -13,6 +16,14 @@ public class PingCommand extends Command {
 
     @Override
     protected void execute(CommandEvent event) {
+
+        long ping = event.getJDA().getGatewayPing();
+
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.setDescription(String.format(":hourglass: %dms", ping));
+        embedBuilder.setColor(Color.GREEN);
+        event.getChannel().sendMessage(embedBuilder.build()).queue();
+
 
     }
 }
