3fbce83dad08529ab2c72a996079c1a04ebefaa3
aleksandrandreichenko <andreichenko@web.de>
2020-12-30 13:01:29 +0300

    feature/mock-tests new tests

--- src/test/java/bot/commands/audio/PlayCommandTest.java
+++ src/test/java/bot/commands/audio/PlayCommandTest.java
@@ -1,4 +1,12 @@
 package bot.commands.audio;
 
 public class PlayCommandTest {
+
+    public static final String MOCK_VOICE_CHANNEL_ID = "mockVoiceChannelId";
+    public static final String MOCK_GUILD_ID = "mockGuildId";
+    public static final String MOCK_MEMBER_ID = "mockMemberId";
+    public static final String MOCK_TEXT_CHANNEL_ID = "mockTextChannelId";
+    public static final String EMPTY_ARGUMENT = "";
+
+
 }
