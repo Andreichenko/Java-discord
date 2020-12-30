7ea3043f28079d0a2cd1496ff237b46fca7a1c9a
aleksandrandreichenko <andreichenko@web.de>
2020-12-30 13:15:08 +0300

    feature/mock-tests new tests

--- src/test/java/bot/commands/audio/PlayCommandTest.java
+++ src/test/java/bot/commands/audio/PlayCommandTest.java
@@ -1,8 +1,10 @@
 package bot.commands.audio;
 
 import bot.commands.audio.utils.AudioPlayerSendHandler;
+import com.jagrosh.jdautilities.command.CommandEvent;
 import bot.commands.audio.utils.TrackSchedulers;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import net.dv8tion.jda.api.entities.VoiceChannel;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +12,9 @@ import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static testUtils.AudioTestMocker.createMockCommandEventForPlayCommandWhereAudioGetsPlayed;
 
 import static org.mockito.Mockito.mock;
 
@@ -39,6 +44,21 @@ public class PlayCommandTest {
         AudioPlayer mockAudioPlayer = mock(AudioPlayer.class);
         AudioPlayerSendHandler audioPlayerSendHandler = new AudioPlayerSendHandler(mockAudioPlayer,
                 new TrackSchedulers());
+
+        CommandEvent mockCommandEvent =
+                createMockCommandEventForPlayCommandWhereVoiceChannelNeedsToBeJoinedAudioGetsPlayed(stringArgumentCaptor,
+                        MOCK_TEXT_CHANNEL_ID, MOCK_MEMBER_ID, MOCK_GUILD_ID, MOCK_VOICE_CHANNEL_ID, COMMAND_ARGUMENT,
+                        audioPlayerSendHandler,
+                        voiceChannelArgumentCaptor);
+
+        AudioPlayerManager playerManager = new DefaultAudioPlayerManager();
+        AudioSourceManagers.registerRemoteSources(playerManager);
+
+        PlayCommand playCommand = new PlayCommand(playerManager);
+        playCommand.execute(mockCommandEvent);
+
+        assertEquals(COMMAND_ARGUMENT, stringArgumentCaptor.getAllValues().get(1));
+        assertEquals(MOCK_VOICE_CHANNEL_ID, voiceChannelArgumentCaptor.getValue().getId());
 
     }
 
