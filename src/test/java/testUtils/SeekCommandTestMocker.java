8d73ffb3589732f6e1d0b35c56310440b9dcc1fb
aleksandrandreichenko <andreichenko@web.de>
2021-01-04 17:33:10 +0300

    feature/mock-tests new tests for bot SeekCommandTestMocker

--- src/test/java/testUtils/SeekCommandTestMocker.java
+++ src/test/java/testUtils/SeekCommandTestMocker.java
@@ -73,6 +73,34 @@ public class SeekCommandTestMocker {
 
     public static CommandEvent createMockCommandEventThatFailsWithTime(ArgumentCaptor<String> stringArgumentCaptor,
                                                                        String timeArgument){
+        MessageAction mockMessageAction = mock(MessageAction.class);
+        doAnswer(invocation -> null).when(mockMessageAction).queue();
 
+        TextChannel mockTextChannel = mock(TextChannel.class);
+        when(mockTextChannel.sendMessage(stringArgumentCaptor.capture())).thenReturn(mockMessageAction);
+
+        AudioTrack mockAudioTrack = mock(AudioTrack.class);
+        when(mockAudioTrack.getDuration()).thenReturn(Long.valueOf(999999999));
+        when(mockAudioTrack.isSeekable()).thenReturn(true);
+
+        AudioPlayer mockAudioPlayer = mock(AudioPlayer.class);
+        when(mockAudioPlayer.getPlayingTrack()).thenReturn(mockAudioTrack);
+
+        AudioPlayerSendHandler mockAudioPlayerSendHandler = mock(AudioPlayerSendHandler.class);
+        when(mockAudioPlayerSendHandler.getAudioPlayer()).thenReturn(mockAudioPlayer);
+
+        AudioManager mockAudioManager = mock(AudioManager.class);
+        when(mockAudioManager.isConnected()).thenReturn(true);
+        when(mockAudioManager.getSendingHandler()).thenReturn(mockAudioPlayerSendHandler);
+
+        Guild mockGuild = mock(Guild.class);
+        when(mockGuild.getAudioManager()).thenReturn(mockAudioManager);
+
+        CommandEvent mockCommandEvent = mock(CommandEvent.class);
+        when(mockCommandEvent.getGuild()).thenReturn(mockGuild);
+        when(mockCommandEvent.getChannel()).thenReturn(mockTextChannel);
+        when(mockCommandEvent.getArgs()).thenReturn(timeArgument);
+
+        return mockCommandEvent;
     }
 }
