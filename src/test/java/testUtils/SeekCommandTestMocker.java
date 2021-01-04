0ed8a24d86175e66471446a77b1e990ccb5146dc
aleksandrandreichenko <andreichenko@web.de>
2021-01-04 17:19:19 +0300

    feature/mock-tests new tests for bot SeekCommandTestMocker

--- src/test/java/testUtils/SeekCommandTestMocker.java
+++ src/test/java/testUtils/SeekCommandTestMocker.java
@@ -2,4 +2,12 @@ package testUtils;
 
 public class SeekCommandTestMocker {
 
+    public static CommandEvent createMockCommandEventWhereBotNotConnected(ArgumentCaptor<String> stringArgumentCaptor) {
+        MessageAction mockMessageAction = mock(MessageAction.class);
+        doAnswer(invocation -> null).when(mockMessageAction).queue();
+
+        TextChannel mockTextChannel = mock(TextChannel.class);
+        when(mockTextChannel.sendMessage(stringArgumentCaptor.capture())).thenReturn(mockMessageAction);
+    }
+
 }
