8b557c4eb7f9f745bfece5f89b5295aa6ec4dc26
aleksandrandreichenko <andreichenko@web.de>
2021-01-04 17:19:50 +0300

    feature/mock-tests new tests for bot SeekCommandTestMocker

--- src/test/java/testUtils/SeekCommandTestMocker.java
+++ src/test/java/testUtils/SeekCommandTestMocker.java
@@ -1,5 +1,19 @@
 package testUtils;
 
+import com.jagrosh.jdautilities.command.CommandEvent;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.managers.AudioManager;
+import net.dv8tion.jda.api.requests.restaction.MessageAction;
+import org.mockito.ArgumentCaptor;
+
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
 public class SeekCommandTestMocker {
 
     public static CommandEvent createMockCommandEventWhereBotNotConnected(ArgumentCaptor<String> stringArgumentCaptor) {
@@ -8,6 +22,17 @@ public class SeekCommandTestMocker {
 
         TextChannel mockTextChannel = mock(TextChannel.class);
         when(mockTextChannel.sendMessage(stringArgumentCaptor.capture())).thenReturn(mockMessageAction);
+
+        AudioManager mockAudioManager = mock(AudioManager.class);
+        when(mockAudioManager.isConnected()).thenReturn(false);
+
+        Guild mockGuild = mock(Guild.class);
+        when(mockGuild.getAudioManager()).thenReturn(mockAudioManager);
+
+        CommandEvent mockCommandEvent = mock(CommandEvent.class);
+        when(mockCommandEvent.getGuild()).thenReturn(mockGuild);
+        when(mockCommandEvent.getChannel()).thenReturn(mockTextChannel);
+        return mockCommandEvent;
     }
 
 }
