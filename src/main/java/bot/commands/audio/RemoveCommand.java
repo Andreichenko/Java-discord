cc44a6b50c6ed0096769c5897a7ccb87a5195052
aleksandrandreichenko <andreichenko@web.de>
2021-01-07 22:37:19 +0300

    feature/mock-tests new tests for bot

---
+++ src/main/java/bot/commands/audio/RemoveCommand.java
@@ -0,0 +1,17 @@
+package bot.commands.audio;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+import net.dv8tion.jda.api.managers.AudioManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import static bot.utils.ChannelTextResponses.REMOVE_COMMAND_NOT_A_NUMBER;
+import static bot.utils.ChannelTextResponses.REMOVE_COMMAND_NO_ARGUMENT;
+import static bot.utils.ChannelTextResponses.REMOVE_COMMAND_NO_TRACK_TO_REMOVE;
+
+public class RemoveCommand {
+
+    private final Logger LOGGER = LogManager.getLogger(RemoveCommand.class);
+
+}
