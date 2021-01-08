c93a66adce15d77cec305fe0fd9553c5a49d3e76
aleksandrandreichenko <andreichenko@web.de>
2021-01-08 16:14:14 +0300

    feature/mock-tests new tests for bot
    created a RemoveCommand

---
+++ src/test/java/bot/commands/audio/SeekCommandTest.java
@@ -0,0 +1,15 @@
+package bot.commands.audio;
+
+import com.jagrosh.jdautilities.command.CommandEvent;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.MockitoAnnotations;
+
+public class SeekCommandTest {
+
+    @Before
+    public void init(){
+        MockitoAnnotations.initMocks(this);
+    }
+}
