0e505be292842d52b6a1d31f538c44286879a682
aleksandrandreichenko <andreichenko@web.de>
2021-01-09 15:56:34 +0300

    feature/mock-tests new tests for bot
    created a RemoveCommand

---
+++ src/main/java/bot/commands/audio/SeekCommand.java
@@ -0,0 +1,15 @@
+package bot.commands.audio;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+import net.dv8tion.jda.api.managers.AudioManager;
+
+public class SeekCommand extends Command {
+
+
+    @Override
+    protected void execute(CommandEvent event) {
+
+        AudioManager audioManager = event.getGuild().getAudioManager();
+    }
+}
