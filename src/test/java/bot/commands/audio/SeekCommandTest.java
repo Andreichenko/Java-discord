a53e4ddcde1614c6a02182419aa34f47c0274822
aleksandrandreichenko <andreichenko@web.de>
2021-01-10 15:57:09 +0300

    feature/mock-tests new tests for bot
    created a RemoveCommand

--- src/test/java/bot/commands/audio/SeekCommandTest.java
+++ src/test/java/bot/commands/audio/SeekCommandTest.java
@@ -45,5 +45,7 @@ public class SeekCommandTest {
         SeekCommand seekCommand = new SeekCommand();
         seekCommand.execute(mockCommandEvent);
 
+        assertEquals(122000, longArgumentCaptor.getValue().longValue());
+
     }
 }
