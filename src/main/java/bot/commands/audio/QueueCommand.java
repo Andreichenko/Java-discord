8c22f59c0670a675176393b42f1157b477d8fcdb
aleksandrandreichenko <andreichenko@web.de>
2021-04-11 02:20:40 +0300

    add hashmap list for restrict some commands added QueueCommand override execute from interface

--- src/main/java/bot/commands/audio/QueueCommand.java
+++ src/main/java/bot/commands/audio/QueueCommand.java
@@ -1,15 +1,28 @@
 package bot.commands.audio;
 
+import bot.commands.audio.utils.AudioPlayerSendHandler;
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.managers.AudioManager;
 
 public class QueueCommand extends Command {
 
     public QueueCommand() {
+        this.name = "queue";
+        this.help = "See the queue for the server";
     }
 
     @Override
     protected void execute(CommandEvent event) {
 
+        AudioManager audioManager = event.getGuild().getAudioManager();
+        MessageChannel channel = event.getChannel();
+
+        AudioPlayerSendHandler audioPlayerSendHandler = (AudioPlayerSendHandler) audioManager.getSendingHandler();
+        if (audioPlayerSendHandler == null){
+            channel.sendMessage("**Queue is empty**").queue();
+            return;
+        }
     }
 }
