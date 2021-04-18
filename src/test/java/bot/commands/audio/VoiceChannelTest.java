f80204cdbdb39395cea57ef2abde9869fa46e48c
aleksandrandreichenko <andreichenko@web.de>
2021-04-18 13:14:59 +0300

    voice channel test

--- src/test/java/bot/commands/audio/VoiceChannelTest.java
+++ src/test/java/bot/commands/audio/VoiceChannelTest.java
@@ -1,4 +1,30 @@
 package bot.commands.audio;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.event.AudioEventListener;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.audio.AudioSendHandler;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.GuildVoiceState;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.VoiceChannel;
+import net.dv8tion.jda.api.managers.AudioManager;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.MockitoAnnotations;
+
 public class VoiceChannelTest {
+
+    @Before
+    public void init(){
+        MockitoAnnotations.initMocks(this);
+    }
 }
