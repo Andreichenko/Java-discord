1dddd9c7b31373279fe23a9599190a85fd92066c
aleksandrandreichenko <andreichenko@web.de>
2021-04-26 14:18:38 +0300

    voice channel test

---
+++ src/test/java/testUtils/Timetest.java
@@ -0,0 +1,15 @@
+package testUtils;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Timetest {
+
+
+    public void TestTimeUtilsWithHoursMinsSeconds()
+    {
+
+    }
+}
