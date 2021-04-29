32ae33010c9e287a4a69e8b614e883b217610414
aleksandrandreichenko <andreichenko@web.de>
2021-04-29 11:59:19 +0300

    test timeutils

--- src/test/java/testUtils/Timetest.java
+++ src/test/java/testUtils/Timetest.java
@@ -7,12 +7,14 @@ import static org.junit.Assert.assertEquals;
 
 public class Timetest {
 
-
     @Test
-    public void TestTimeUtilsWithHoursMinsSeconds()
-    {
+    public void TestTimeUtilsWithHoursMinsSeconds() {
         // This is 1 hour 1 minute and 40 seconds
         String timeString = TimeUtils.timeString(3700);
         assertEquals("1:01:40", timeString);
+    }
+
+    public void TestTimeUtilsWithMinimalsSeconds(){
+
     }
 }
