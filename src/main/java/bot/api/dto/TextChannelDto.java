3817a4399aa353af9a0c48acd9af07949c2315be
andreichenko <andreichenko@web.de>
2022-05-06 22:00:36 +0300

    DTOs or Data Transfer Objects are objects that carry data between processes in order to reduce the number of methods calls.

---
+++ src/main/java/bot/api/dto/TextChannelDto.java
@@ -0,0 +1,27 @@
+package bot.api.dto;
+
+public class TextChannelDto {
+    public String name;
+    public String id;
+
+    public TextChannelDto(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
