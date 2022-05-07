df235e9bacec46ce55fdd3131bfea946641b554c
andreichenko <andreichenko@web.de>
2022-05-07 18:59:55 +0300

    CommandEvent interface

--- src/main/java/bot/utils/commands/CommandEvent.java
+++ src/main/java/bot/utils/commands/CommandEvent.java
@@ -2,6 +2,7 @@ package bot.utils.commands;
 
 import com.jagrosh.jdautilities.command.CommandClient;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 import java.util.function.Consumer;
@@ -13,6 +14,8 @@ public interface CommandEvent {
     CommandClient getClient();
 
     void linkId(Message message);
+    void reply(String message, Consumer<Message> success, Consumer<Throwable> failure);
+    void reply(MessageEmbed embed);
     void reply(String message);
     void reply(String message, Consumer<Message> success);
 }
