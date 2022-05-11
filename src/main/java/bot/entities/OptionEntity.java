cf5d09723115a6b4a9218328a7fa9940335064b3
andreichenko <andreichenko@web.de>
2022-05-11 22:46:54 +0300

    api rework and fix

--- src/main/java/bot/entities/OptionEntity.java
+++ src/main/java/bot/entities/OptionEntity.java
@@ -1,6 +1,11 @@
 package bot.entities;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 
 @Entity
 public class OptionEntity {
@@ -8,7 +13,6 @@ public class OptionEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     private String serverId;
     private String name;
 
