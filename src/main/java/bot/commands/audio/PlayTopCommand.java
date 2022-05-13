6053b438e3f9ee4d6b9d3c07b551f28ab3d6baf8
andreichenko <andreichenko@web.de>
2022-05-13 19:23:15 +0300

    Add in option for autoplay and begin API rewrite del test classes

--- src/main/java/bot/commands/audio/PlayTopCommand.java
+++ src/main/java/bot/commands/audio/PlayTopCommand.java
@@ -1,25 +1,29 @@
 package bot.commands.audio;
 
 import bot.commands.audio.utils.VoiceChannelUtils;
-import com.jagrosh.jdautilities.command.Command;
-import com.jagrosh.jdautilities.command.CommandEvent;
+import bot.repositories.OptionEntityRepository;
+import bot.utils.commands.Command;
+import bot.utils.commands.CommandEvent;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 
 public class PlayTopCommand extends Command {
-
     private final AudioPlayerManager playerManager;
+    private final String youtubeApiKey;
+    private final OptionEntityRepository optionEntityRepository;
 
-    public PlayTopCommand(AudioPlayerManager playerManager)
-    {
+    public PlayTopCommand(AudioPlayerManager playerManager, String youtubeApiKey,
+                          OptionEntityRepository optionEntityRepository) {
         this.playerManager = playerManager;
         this.name = "playtop";
         this.aliases = new String[]{"pt"};
         this.help = "Plays a song with the given name or url by placing it at the top of the queue.";
+
+        this.youtubeApiKey = youtubeApiKey;
+        this.optionEntityRepository = optionEntityRepository;
     }
 
     @Override
-    protected void execute(CommandEvent event)
-    {
-        VoiceChannelUtils.SearchAndPlaySong(event.getJDA(), event.getArgs(), event.getGuild().getId(), event.getChannel().getId(), event.getMember().getId(), true, playerManager);
+    protected void execute(CommandEvent event) {
+        VoiceChannelUtils.searchAndPlaySong(event, true, playerManager, youtubeApiKey, optionEntityRepository);
     }
 }
