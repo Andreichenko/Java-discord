3281f99b04085f010c68006531c2c182d549fa4b
andreichenko <andreichenko@web.de>
2022-05-13 18:55:44 +0300

    Add in option for autoplay and begin API rewrite del test classes

--- src/main/java/bot/commands/audio/PauseCommand.java
+++ src/main/java/bot/commands/audio/PauseCommand.java
@@ -1,35 +1,34 @@
 package bot.commands.audio;
 
 import bot.commands.audio.utils.VoiceChannelUtils;
-import com.jagrosh.jdautilities.command.Command;
-import com.jagrosh.jdautilities.command.CommandEvent;
+import bot.utils.TextChannelResponses;
+import bot.utils.UnicodeEmote;
+import bot.utils.commands.Command;
+import bot.utils.commands.CommandEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class PauseCommand extends Command {
-
     private final Logger LOGGER = LogManager.getLogger(PauseCommand.class);
 
-    public PauseCommand(){
+    public PauseCommand() {
         this.name = "pause";
         this.help = "Pause the currently playing song.";
     }
 
     @Override
-    protected void execute(CommandEvent event){
-
+    protected void execute(CommandEvent event) {
         try {
-            VoiceChannelUtils.setPauseStatusOnAudioPlayer(event.getGuild(), event.getChannel(), event.getMember(), true);
-        }catch(IllegalArgumentException e){
+            VoiceChannelUtils.setPauseStatusOnAudioPlayer(event, true);
+        } catch(IllegalArgumentException e) {
             LOGGER.debug("Trying to pause a paused song", e);
             event.getChannel().sendMessage(TextChannelResponses.TRYING_TO_PAUSE_PAUSED_SONG).queue();
             return;
-        } catch(IllegalAccessException e){
+        }
+        catch(IllegalAccessException e) {
             LOGGER.debug("Error while running pause command", e);
             return;
         }
-
-        event.getMessage().addReaction(UnicodeMotion.THUMBS_UP).queue();
-
+        event.getMessage().addReaction(UnicodeEmote.THUMBS_UP).queue();
     }
 }
--- src/main/java/bot/commands/audio/PlayCommand.java
+++ src/main/java/bot/commands/audio/PlayCommand.java
@@ -1,32 +1,31 @@
 package bot.commands.audio;
 
 import bot.commands.audio.utils.VoiceChannelUtils;
-import com.jagrosh.jdautilities.command.Command;
-import com.jagrosh.jdautilities.command.CommandEvent;
+import bot.repositories.OptionEntityRepository;
+import bot.utils.commands.Command;
+import bot.utils.commands.CommandEvent;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class PlayCommand extends Command{
-
-
+public class PlayCommand extends Command {
+    private static final Logger LOGGER = LogManager.getLogger(PlayCommand.class);
     private final AudioPlayerManager playerManager;
+    private final String youtubeApiKey;
+    private final OptionEntityRepository optionEntityRepository;
 
-    private final Logger LOGGER = LogManager.getLogger(PlayCommand.class);
-
-    public PlayCommand(AudioPlayerManager playerManager) {
+    public PlayCommand(AudioPlayerManager playerManager, String youtubeApiKey, OptionEntityRepository optionEntityRepository) {
         this.playerManager = playerManager;
         this.name = "play";
-        this.help = "Please submit a song with the specified name or url.";
+        this.help = "Plays a song with the given name or url.";
+
+        this.youtubeApiKey = youtubeApiKey;
+        this.optionEntityRepository = optionEntityRepository;
     }
 
     @Override
     protected void execute(CommandEvent event) {
-
         LOGGER.info("Play command triggered with message {}", event.getArgs());
-        VoiceChannelUtils.SearchAndPlaySong(event.getJDA(), event.getArgs(), event.getGuild().getId(),
-                event.getChannel().getId(), event.getMember().getId(), false, playerManager);
+        VoiceChannelUtils.searchAndPlaySong(event, false, playerManager, youtubeApiKey, optionEntityRepository);
     }
-
-
 }
