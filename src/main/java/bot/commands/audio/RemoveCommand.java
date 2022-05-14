a0ae7e470bf86aa43d8404a1f26a231e90cbfec1
andreichenko <andreichenko@web.de>
2022-05-14 17:03:49 +0300

    make code more readable

--- src/main/java/bot/commands/audio/RemoveCommand.java
+++ src/main/java/bot/commands/audio/RemoveCommand.java
@@ -2,9 +2,11 @@ package bot.commands.audio;
 
 import bot.commands.audio.utils.AudioPlayerSendHandler;
 import bot.commands.audio.utils.TrackSchedulers;
-import com.jagrosh.jdautilities.command.Command;
-import com.jagrosh.jdautilities.command.CommandEvent;
+import bot.utils.UnicodeEmote;
+import bot.utils.commands.Command;
+import bot.utils.commands.CommandEvent;
 import net.dv8tion.jda.api.managers.AudioManager;
+import net.dv8tion.jda.api.requests.RestAction;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -13,28 +15,23 @@ import static bot.utils.TextChannelResponses.REMOVE_COMMAND_NO_ARGUMENT;
 import static bot.utils.TextChannelResponses.REMOVE_COMMAND_NO_TRACK_TO_REMOVE;
 
 public class RemoveCommand extends Command {
-
     private final Logger LOGGER = LogManager.getLogger(RemoveCommand.class);
 
-
-    public RemoveCommand(){
+    public RemoveCommand() {
         this.name = "remove";
         this.help = "Remove the requested song from the queue.";
     }
 
     @Override
     protected void execute(CommandEvent event) {
-
         String argument = event.getArgs();
 
-        if (argument.isEmpty()){
-
+        if (argument.isEmpty()) {
             event.getChannel().sendMessage(REMOVE_COMMAND_NO_ARGUMENT).queue();
             return;
         }
 
         int trackToRemove;
-
         try {
             trackToRemove = Integer.parseInt(argument);
         } catch(NumberFormatException e) {
@@ -45,11 +42,9 @@ public class RemoveCommand extends Command {
 
         AudioManager audioManager = event.getGuild().getAudioManager();
 
-
         AudioPlayerSendHandler audioPlayerSendHandler = (AudioPlayerSendHandler) audioManager.getSendingHandler();
 
-        if (audioPlayerSendHandler == null){
-
+        if (audioPlayerSendHandler == null) {
             event.getChannel().sendMessage(String.format(REMOVE_COMMAND_NO_TRACK_TO_REMOVE, trackToRemove)).queue();
             return;
         }
@@ -58,12 +53,11 @@ public class RemoveCommand extends Command {
 
         try {
             trackScheduler.remove(trackToRemove - 1);
-        }catch(IndexOutOfBoundsException e){
+        } catch(IndexOutOfBoundsException e) {
             LOGGER.info("Track {} is not a track on the queue", trackToRemove);
             event.getChannel().sendMessage(String.format(REMOVE_COMMAND_NO_TRACK_TO_REMOVE, trackToRemove)).queue();
             return;
         }
-        event.getMessage().addReaction(UnicodeMotion.THUMBS_UP).queue();
-
+        event.getMessage().addReaction(UnicodeEmote.THUMBS_UP).queue();
     }
 }
