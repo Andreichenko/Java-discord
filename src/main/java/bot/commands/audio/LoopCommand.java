6886d09292aee64665565bf675a9eb456f5ef36d
andreichenko <andreichenko@web.de>
2022-05-15 17:04:07 +0300

    api rewrite

--- src/main/java/bot/commands/audio/LoopCommand.java
+++ src/main/java/bot/commands/audio/LoopCommand.java
@@ -1,5 +1,6 @@
 package bot.commands.audio;
 
+import bot.commands.audio.utils.AudioPlayerSendHandler;
 import bot.utils.commands.Command;
 import bot.utils.commands.CommandEvent;
 import org.apache.logging.log4j.LogManager;
@@ -8,8 +9,13 @@ import org.apache.logging.log4j.Logger;
 public class LoopCommand extends Command {
     private final Logger LOGGER = LogManager.getLogger(LoopCommand.class);
 
+    public LoopCommand() {
+        this.name = "loop";
+        this.help = "Enable or disable the current song from looping.";
+    }
+
     @Override
     protected void execute(CommandEvent event) {
-
+        AudioPlayerSendHandler audioPlayerSendHandler;
     }
 }
