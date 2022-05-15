45fcf88271d67b94cc675f432383d039b02d58d3
andreichenko <andreichenko@web.de>
2022-05-15 17:02:02 +0300

    api rewrite

--- src/main/java/bot/commands/audio/LoopCommand.java
+++ src/main/java/bot/commands/audio/LoopCommand.java
@@ -1,4 +1,8 @@
 package bot.commands.audio;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class LoopCommand {
+    private final Logger LOGGER = LogManager.getLogger(LoopCommand.class);
 }
