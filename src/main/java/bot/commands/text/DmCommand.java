e8ab16ccb0041d74bc991f0f61fb8dc08236921a
andreichenko <andreichenko@web.de>
2022-05-18 17:59:04 +0300

    echo text for api rework phase 2.5

--- src/main/java/bot/commands/text/DmCommand.java
+++ src/main/java/bot/commands/text/DmCommand.java
@@ -8,11 +8,20 @@ import org.apache.logging.log4j.Logger;
 
 public class DmCommand extends Command{
     public DmCommand() {
+        this.name = "dm";
+        this.hidden = true;
+        this.allowedInGuild = false;
+        this.allowedInDm = true;
     }
 
     @Override
     protected void execute(CommandEvent event) {
         String rawContent = event.getMessage().getContentRaw();
+        String[] queryParts = rawContent.split("\\s+");
+    }
+
+    private void fail(CommandEvent event){
+        event.getAuthor().openPrivateChannel().queue(channel -> channel.sendMessage("noob").queue());
     }
 }
 
