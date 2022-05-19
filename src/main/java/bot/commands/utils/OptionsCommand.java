cd4cce217077f3b47fdc012b4e0f8f7c9cd86ea3
andreichenko <andreichenko@web.de>
2022-05-19 21:12:08 +0300

    api rework phase 2.5

--- src/main/java/bot/commands/utils/OptionsCommand.java
+++ src/main/java/bot/commands/utils/OptionsCommand.java
@@ -7,13 +7,17 @@ import bot.utils.commands.Command;
 import bot.utils.commands.CommandEvent;
 import org.springframework.stereotype.Component;
 
-
 import java.util.HashMap;
 import java.util.Locale;
+
+import static bot.utils.OptionsCommands.AUTOPLAY_HELP;
+import static bot.utils.OptionsCommands.AUTOPLAY_NAME;
+
 
 @Component
 public class OptionsCommand extends Command{
 
+    static final String[] OPTION_NAMES = new String[]{AUTOPLAY_NAME};
     private final OptionEntityRepository optionEntityRepository;
     private final HashMap<Boolean, String> enabledDisabledMap = new HashMap<>();
 
@@ -22,10 +26,19 @@ public class OptionsCommand extends Command{
 
         this.enabledDisabledMap.put(Boolean.FALSE, "disabled");
         this.enabledDisabledMap.put(Boolean.TRUE, "enabled");
+        this.name = "option";
+        this.aliases = new String[]{"setting", "settings"};
+        this.help = "Set options for the bot. Current Options:\n" +
+                String.format("> %s - %s", AUTOPLAY_NAME, AUTOPLAY_HELP);
     }
     @Override
     protected void execute(CommandEvent event) {
         event.getChannel().sendTyping().queue();
+
+        //command is given as -options OPTIONS_NAME <optional true/false value>
+        //get the arguments and extract them into the different parts
+        String[] arguments = event.getArgs().split("\\s+");
+
         String guildId = event.getGuild().getId();
         String optionName = arguments[0].toLowerCase(Locale.ROOT);
         OptionEntity optionEntity = optionEntityRepository.findByServerIdAndName(guildId, optionName);
