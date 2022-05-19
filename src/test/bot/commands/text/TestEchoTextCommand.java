8477a4c4118f569bb929c4f29a0f4836fb9a9aea
andreichenko <andreichenko@web.de>
2022-05-19 23:47:28 +0300

    api rework phase 2.5 +tests

---
+++ src/test/bot/commands/text/TestEchoTextCommand.java
@@ -0,0 +1,11 @@
+package bot.commands.text;
+
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestEchoTextCommand {
+
+}
