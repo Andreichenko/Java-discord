e39ff9787fb1376b1f9ceacbd11de67799561992
andreichenko <andreichenko@web.de>
2022-05-19 23:06:00 +0300

    api rework phase 2.5

--- src/main/java/bot/commands/utils/OptionListCommand.java
+++ src/main/java/bot/commands/utils/OptionListCommand.java
@@ -1,5 +1,6 @@
 package bot.commands.utils;
 
+import bot.entities.OptionEntity;
 import bot.repositories.OptionEntityRepository;
 import bot.utils.commands.Command;
 import bot.utils.commands.CommandEvent;
@@ -7,6 +8,9 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
+
+import static bot.commands.utils.OptionsCommand.OPTION_NAMES;
+import static bot.utils.EmbedUtils.setRandomColour;
 
 @Component
 public class OptionListCommand extends Command {
@@ -28,6 +32,19 @@ public class OptionListCommand extends Command {
 
         HashMap<String, Boolean> nameToState = new HashMap<>();
 
+        for (String option : OPTION_NAMES) {
+            OptionEntity optionEntity = optionEntityRepository.findByServerIdAndName(event.getGuild().getId(), option);
+
+            if (optionEntity == null || optionEntity.getOption()) {
+                nameToState.put(option, true);
+
+            } else {
+                nameToState.put(option, false);
+            }
+        }
+
         EmbedBuilder embedBuilder = new EmbedBuilder();
+
+        setRandomColour(embedBuilder);
     }
 }
