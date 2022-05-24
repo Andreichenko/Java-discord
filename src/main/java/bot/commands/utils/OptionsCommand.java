bf4c8c37b3922ae305891601a13514c96dc642cf
andreichenko <andreichenko@web.de>
2022-05-24 14:22:38 +0300

    api rework phase 2.5

--- src/main/java/bot/commands/utils/OptionsCommand.java
+++ src/main/java/bot/commands/utils/OptionsCommand.java
@@ -1,5 +1,4 @@
-package bot.commands.utils;
-
+package bot.commands.utilities;
 
 import bot.entities.OptionEntity;
 import bot.repositories.OptionEntityRepository;
@@ -13,21 +12,23 @@ import java.util.Locale;
 
 import static bot.utils.OptionsCommands.AUTOPLAY_HELP;
 import static bot.utils.OptionsCommands.AUTOPLAY_NAME;
-import static bot.utils.TextChannelResponses.*;
-
+import static bot.utils.TextChannelResponses.NEED_MORE_ARGUMENTS_TO_SET_OPTION;
+import static bot.utils.TextChannelResponses.NOT_VALID_BOOLEAN;
+import static bot.utils.TextChannelResponses.NOT_VALID_OPTION;
 
 @Component
-public class OptionsCommand extends Command{
-
+public class OptionsCommand extends Command
+{
     static final String[] OPTION_NAMES = new String[]{AUTOPLAY_NAME};
     private final OptionEntityRepository optionEntityRepository;
     private final HashMap<Boolean, String> enabledDisabledMap = new HashMap<>();
 
-    public OptionsCommand(OptionEntityRepository optionEntityRepository){
+    public OptionsCommand(OptionEntityRepository optionEntityRepository) {
         this.optionEntityRepository = optionEntityRepository;
 
         this.enabledDisabledMap.put(Boolean.FALSE, "disabled");
         this.enabledDisabledMap.put(Boolean.TRUE, "enabled");
+
         this.name = "option";
         this.aliases = new String[]{"setting", "settings"};
         this.help = "Set options for the bot. Current Options:\n" +
@@ -37,11 +38,11 @@ public class OptionsCommand extends Command{
     @Override
     protected void execute(CommandEvent event) {
         event.getChannel().sendTyping().queue();
-        // if an argument was not provided and there is an optionEntity then just invert whatever is currently set.
-        // also we have to check that at least 3 arguments are specified
+
         //command is given as -options OPTIONS_NAME <optional true/false value>
         //get the arguments and extract them into the different parts
         String[] arguments = event.getArgs().split("\\s+");
+
         //check that at least 3 arguments are specified
         if (arguments.length < 1 || arguments[0].equals("")) {
             event.getChannel().sendMessage(NEED_MORE_ARGUMENTS_TO_SET_OPTION).queue();
@@ -55,8 +56,9 @@ public class OptionsCommand extends Command{
             event.getChannel().sendMessage(String.format(NOT_VALID_OPTION, optionName)).queue();
             return;
         }
+
         Boolean booleanValue = null;
-        // ??????  check if an argument was provided
+        // check if an argument was provided
         if (arguments.length == 2) {
             String booleanSetter = arguments[1].toLowerCase(Locale.ROOT);
 
@@ -67,6 +69,7 @@ public class OptionsCommand extends Command{
 
             booleanValue = Boolean.parseBoolean(booleanSetter);
         }
+
         OptionEntity optionEntity = optionEntityRepository.findByServerIdAndName(guildId, optionName);
 
         // if an argument was not provided and there is an optionEntity then just invert whatever is currently set.
@@ -84,5 +87,11 @@ public class OptionsCommand extends Command{
                 booleanValue = Boolean.FALSE;
             }
         }
+
+        optionEntity.setOption(booleanValue);
+        optionEntityRepository.save(optionEntity);
+        String disabled_enabled_text = enabledDisabledMap.get(booleanValue);
+
+        event.getChannel().sendMessage(String.format("**%s has been %s.**", optionName, disabled_enabled_text)).queue();
     }
 }
