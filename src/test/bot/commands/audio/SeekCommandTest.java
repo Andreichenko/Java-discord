e3677a0741d88dad20c56d83e17a64f3e2bc6b66
andreichenko <andreichenko@web.de>
2022-05-25 13:35:06 +0300

    api rework phase 2.5 + test

---
+++ src/test/bot/commands/audio/SeekCommandTest.java
@@ -0,0 +1,12 @@
+package bot.commands.audio;
+
+import org.junit.Before;
+import org.mockito.MockitoAnnotations;
+
+public class SeekCommandTest {
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+}
