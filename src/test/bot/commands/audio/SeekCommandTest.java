e8a6ab5dc4e3a90b755441a17fb501f0d3ef4c0a
andreichenko <andreichenko@web.de>
2022-05-26 16:52:59 +0300

    api rework phase 2.5 + test

--- src/test/bot/commands/audio/SeekCommandTest.java
+++ src/test/bot/commands/audio/SeekCommandTest.java
@@ -7,6 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.MockitoAnnotations;
+import testUtils.SeekCommandTestMocker;
 
 import static org.junit.Assert.assertEquals;
 
@@ -17,11 +18,17 @@ public class SeekCommandTest {
         MockitoAnnotations.initMocks(this);
     }
 
+    @Test
     public void seekWithHoursMinutesSeconds() {
         ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
         ArgumentCaptor<Long> longArgumentCaptor = ArgumentCaptor.forClass(Long.class);
-        SeekCommand seekCommand = new SeekCommand();
+
 // check
+        CommandEvent mockCommandEvent = SeekCommandTestMocker.createMockCommandEventWithTime(stringArgumentCaptor,
+                longArgumentCaptor, "3:02:02");
+        SeekCommand seekCommand = new SeekCommand();
+        seekCommand.execute(mockCommandEvent);
+
         assertEquals(10922000, longArgumentCaptor.getValue().longValue());
         assertEquals(String.format(TextChannelResponses.SEEKING_TO_INFORMATION, TimeUtils.timeString(10922)),
                 stringArgumentCaptor.getValue());
