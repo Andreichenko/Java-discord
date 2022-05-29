a79c77ea733f61d959fa845755203ab284b02104
andreichenko <andreichenko@web.de>
2022-05-29 14:34:18 +0300

    api rework phase 3 tests

--- src/test/testUtils/SeekCommandTestMocker.java
+++ src/test/testUtils/SeekCommandTestMocker.java
@@ -86,6 +86,10 @@ public class SeekCommandTestMocker {
 
     public static CommandEvent createMockCommandEventThatFailsSongNotSeekable(ArgumentCaptor<String> stringArgumentCaptor,
                                                                               String timeArgument){
+        MessageAction mockMessageAction = mock(MessageAction.class);
+        doAnswer(invocation -> null).when(mockMessageAction).queue();
+
+
         return null;
     }
 
