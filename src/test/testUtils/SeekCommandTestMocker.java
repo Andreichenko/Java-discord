3b89ad3264c88f40c0a86fb7bd17a2ea38052ebe
andreichenko <andreichenko@web.de>
2022-05-29 14:35:56 +0300

    api rework phase 3 tests

--- src/test/testUtils/SeekCommandTestMocker.java
+++ src/test/testUtils/SeekCommandTestMocker.java
@@ -76,6 +76,11 @@ public class SeekCommandTestMocker {
 
         TextChannel mockTextChannel = mock(TextChannel.class);
         when(mockTextChannel.sendMessage(stringArgumentCaptor.capture())).thenReturn(mockMessageAction);
+
+        AudioTrack mockAudioTrack = mock(AudioTrack.class);
+        when(mockAudioTrack.getDuration()).thenReturn(Long.valueOf(999999999));
+        when(mockAudioTrack.isSeekable()).thenReturn(true);
+
         return null;
     }
 
