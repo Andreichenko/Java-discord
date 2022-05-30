835436c3ecf205e28723ccec2b99e5532d7d65f5
andreichenko <andreichenko@web.de>
2022-05-30 14:14:48 +0300

    api rework phase 3 tests

--- src/test/bot/commands/text/TestEchoTextCommand.java
+++ src/test/bot/commands/text/TestEchoTextCommand.java
@@ -1,10 +1,14 @@
 package bot.commands.text;
 
 
+import net.dv8tion.jda.api.entities.TextChannel;
 import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import static bot.utils.MockTextChannelCreator.createMockTextChannelWhereTextIsSentNoTyping;
 
 
 @RunWith(MockitoJUnitRunner.class)
@@ -16,6 +20,8 @@ public class TestEchoTextCommand {
     }
 
     public void testFailsWhenNoArgsAreSent(){
+        ArgumentCaptor<String> textChannelArgumentCaptor = ArgumentCaptor.forClass(String.class);
+        TextChannel mockTextChannel = createMockTextChannelWhereTextIsSentNoTyping(textChannelArgumentCaptor);
 
     }
 
