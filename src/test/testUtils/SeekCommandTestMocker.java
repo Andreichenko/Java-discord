8f32409542392c7e41b4685f3f45dd87a0d5806e
andreichenko <andreichenko@web.de>
2022-06-02 21:37:33 +0300

    api rework phase 3 tests

--- src/test/testUtils/SeekCommandTestMocker.java
+++ src/test/testUtils/SeekCommandTestMocker.java
@@ -148,6 +148,9 @@ public class SeekCommandTestMocker {
         MessageAction mockMessageAction = mock(MessageAction.class);
         doAnswer(invocation -> null).when(mockMessageAction).queue();
 
+        TextChannel mockTextChannel = mock(TextChannel.class);
+        when(mockTextChannel.sendMessage(stringArgumentCaptor.capture())).thenReturn(mockMessageAction);
+
         return null;
     }
 
