0a111517ab596f9e4d6559780f6696c6514ae607
andreichenko <andreichenko@web.de>
2022-06-08 15:39:01 +0300

    api rework phase 3 tests

--- src/test/bot/commands/text/TestEchoTextCommand.java
+++ src/test/bot/commands/text/TestEchoTextCommand.java
@@ -10,7 +10,7 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import static bot.utils.MockTextChannelCreator.createMockTextChannelWhereTextIsSentNoTyping;
+//import static bot.utils.MockTextChannelCreator.createMockTextChannelWhereTextIsSentNoTyping;
 import static bot.utils.TextChannelResponses.ECHO_COMMAND_NO_ARGS;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
