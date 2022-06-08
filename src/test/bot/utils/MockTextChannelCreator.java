558deb07f52a7cd235bf0a3254ff224b1ef3131e
andreichenko <andreichenko@web.de>
2022-06-08 15:34:46 +0300

    api rework phase 3 tests

--- src/test/bot/utils/MockTextChannelCreator.java
+++ src/test/bot/utils/MockTextChannelCreator.java
@@ -10,13 +10,13 @@ import static org.mockito.Mockito.when;
 
 public class MockTextChannelCreator {
 
-    public static TextChannel createMockTextChannelWhereTextIsSentNoTyping(ArgumentCaptor<String> stringArgumentCaptor){
-        MessageAction mockMessageAction = mock(MessageAction.class);
-        doAnswer(invocation -> null).when(mockMessageAction).queue();
-
-        TextChannel mockTextChannel = mock(TextChannel.class);
-        when(mockTextChannel.sendMessage(stringArgumentCaptor.capture())).thenReturn(mockMessageAction);
-
-        return mockTextChannel;
-    }
+//    public static TextChannel createMockTextChannelWhereTextIsSentNoTyping(ArgumentCaptor<String> stringArgumentCaptor){
+//        MessageAction mockMessageAction = mock(MessageAction.class);
+//        doAnswer(invocation -> null).when(mockMessageAction).queue();
+//
+//        TextChannel mockTextChannel = mock(TextChannel.class);
+//        when(mockTextChannel.sendMessage(stringArgumentCaptor.capture())).thenReturn(mockMessageAction);
+//
+//        return mockTextChannel;
+//    }
 }
