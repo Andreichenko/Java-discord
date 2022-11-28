bbac74c7331e13eb9f4370b171c0420e910c3004
aleksandrandreichenko <andreichenko@web.de>
2022-11-28 20:13:22 +0200

    Recreate all code

---
+++ src/test/bot/commands/audio/utils/PlayCommandTest.java
@@ -0,0 +1,24 @@
+package bot.commands.audio.utils;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+
+public class PlayCommandTest {
+
+    public static final String MOCK_VOICE_CHANNEL_ID = "mockVoiceChannelId";
+    public static final String MOCK_GUILD_ID = "mockGuildId";
+    public static final String MOCK_MEMBER_ID = "mockMemberId";
+    public static final String MOCK_TEXT_CHANNEL_ID = "mockTextChannelId";
+    public static final String EMPTY_ARGUMENT = "";
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+}
